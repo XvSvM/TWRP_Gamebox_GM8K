d2fe82ed963deea566ab115d98cf4d95e3e520d2
XvSvM <vadiagadia@gmail.com>
2024-06-08 02:32:27 +0300

    Update and rename twrp_GM8K.mk to eros-p1.mk

--- eros-p1.mk
+++ eros-p1.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from GM8K device
-$(call inherit-product, device/gamebox/GM8K/device.mk)
+$(call inherit-product, device/google/eros-p1/device.mk)
 
 PRODUCT_DEVICE := eros-p1
 PRODUCT_NAME := GameBox
@@ -20,10 +20,9 @@ PRODUCT_BRAND := google
 PRODUCT_MODEL := GameBox
 PRODUCT_MANUFACTURER := Google
 
-PRODUCT_GMS_CLIENTID_BASE := android-gamebox
+PRODUCT_GMS_CLIENTID_BASE := android-google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="Allwinner/eros_p1/eros-p1:10/QP1A.191105.004/413:eng/test-keys"
 
 BUILD_FINGERPRINT := google/walleye/walleye:10/QP1A.191105.004/5908170:user/release-keys
-
