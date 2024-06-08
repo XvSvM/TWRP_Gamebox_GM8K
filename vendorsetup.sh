a01d781e5780f233e3c905d5113f871cbf94c87b
XvSvM <vadiagadia@gmail.com>
2024-06-08 03:56:56 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo eros-p1-user
-add_lunch_combo eros-p1-userdebug
-add_lunch_combo eros-p1-eng
+add_lunch_combo omni_Gamebox-user
+add_lunch_combo omni_Gamebox-userdebug
+add_lunch_combo omni_Gamebox-eng
