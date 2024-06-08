cfdb546aa34819c9ebd031348a87b13362ae8237
XvSvM <vadiagadia@gmail.com>
2024-06-08 11:44:17 +0300

    Update omni_Gamebox.mk

--- omni_Gamebox.mk
+++ omni_Gamebox.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/google/Gamebox/device.mk)
 
 PRODUCT_DEVICE := eros-p1
-PRODUCT_NAME := omni_GameBox
+PRODUCT_NAME := twrp_Gamebox
 PRODUCT_BRAND := google
 PRODUCT_MODEL := GameBox
 PRODUCT_MANUFACTURER := Google
